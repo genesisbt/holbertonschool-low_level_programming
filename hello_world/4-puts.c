90007088a26c92f5f2be28a64a431faec5e001a9
German <gerchureloco@gmail.com>
2023-10-11 08:58:31 -0700

    Excercises 8 to 10 correction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- * Main - excersise 4
+ * main - excercise
  *
  * Description: Using puts
  *
  * Return: does not return nothing at all
  */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
