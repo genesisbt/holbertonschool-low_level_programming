a1fd73e51519de77206d79cdfabaab6b90f76d7b
German <gerchureloco@gmail.com>
2023-10-11 08:45:22 -0700

    Excercises 8 to 10 correction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,7 +5,7 @@
  * return nothing at all
 */
 
-int main () {
-puts ("\"Programming is like building a multilingual puzzle");
-	return(0);
+int main (void) {
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
