ad368dd590b51c4f66ac2305d800dbb19892d2a6
German <gerchureloco@gmail.com>
2023-10-11 10:18:54 -0700

    11/10/2023

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - excercise positive or negative
+ *
+ * Description: This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d is positive\n", n);
+	if (n > 0)
+		printf("%d is negative\n", n);
+	return (0);
+}
