62802dedbe640103e92d3c58fc93166493783ee4
German <gerchureloco@gmail.com>
2023-10-11 09:05:45 -0700

    11/10/2023

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - excercise
+ *
+ * Description: Using puts
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
