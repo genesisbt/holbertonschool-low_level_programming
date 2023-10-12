4e6d956a17ef04c5a46ddcfc198b530a4c4f3f0f
German <gerchureloco@gmail.com>
2023-10-12 07:36:53 -0700

    12/10/2023

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - excercise
+ *
+ * Description: Using puts
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+printf("_putchar");
+putchar('\n');
+return (0);
+}
