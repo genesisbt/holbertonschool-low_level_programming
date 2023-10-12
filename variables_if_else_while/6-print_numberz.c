8121d983334016efcd2c52f08dafeba15b856e54
German <gerchureloco@gmail.com>
2023-10-12 06:41:42 -0700

    12/10/2023

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - excercise five variables
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+putchar('\n');
+return (0);
+}
