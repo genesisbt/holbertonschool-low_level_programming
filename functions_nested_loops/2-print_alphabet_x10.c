6d633862635f725f39bd43300d6e3f87fba643d4
German <gerchureloco@gmail.com>
2023-10-12 09:40:17 -0700

    12/10/2023

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - excercise two
+ *
+ * Description: functions nested loops
+ *
+ * Return: does not return nothing at all
+ */
+
+int print_alphabet_x10(void)
+{
+	char letra;
+	int num;
+
+	for (num = 0 ; num <10 ; num++)
+	{
+	for (letra = 'a' ; letra <= 'z'; letra++)
+	{
+		_putchar (letra);
+	}
+	}
+_putchar('\n');
+return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,6 @@
 int _putchar(char c);
 int imprimir(void);
 int print_alphabet(void);
+int print_alphabet_x10(void);
 
 #endif
