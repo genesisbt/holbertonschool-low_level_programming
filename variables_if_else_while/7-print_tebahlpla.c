8b08d062322b70bb2960b5bbc3c7edbf962f8b8a
German <gerchureloco@gmail.com>
2023-10-12 06:58:28 -0700

    12/10/2023

---
+++ variables_if_else_while/7-print_tebahlpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - excercise
+ *
+ * Description: Using puts
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+	char letra;
+
+	for (letra = 'z' ; letra >= 'a'; letra--)
+	{
+	if (letra != 'q' && letra != 'e')
+	{
+		putchar (letra);
+	}
+	}
+putchar('\n');
+return (0);
+}
