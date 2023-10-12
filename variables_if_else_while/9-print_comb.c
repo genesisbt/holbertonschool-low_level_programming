dbf212b37d9dc38448594a8a00078cc52aead74e
German <gerchureloco@gmail.com>
2023-10-12 07:17:10 -0700

    12/10/2023

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - excercise five variables
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+	int num;
+	int num2;
+
+	for (num = 0; num <= 9; num++)
+	{
+		for (num2 = 0; num <=9 ; num2++)
+		{
+			putchar(num);
+			putchar(num2);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+return (0);
+}
