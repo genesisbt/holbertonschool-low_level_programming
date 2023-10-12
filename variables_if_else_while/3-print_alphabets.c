2327bbb6aff5c5b9d38e1e2db857ab2f284ca138
German <gerchureloco@gmail.com>
2023-10-12 06:13:03 -0700

    12/10/2023

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - excercise
+ *
+ * Description: Using puts
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(void)
+{
+	char letra;
+
+	for (letra = 'a' ; letra <= 'Z'; letra++)
+	{
+	putchar(letra);
+	}
+putchar('\n');
+return (0);
+}
