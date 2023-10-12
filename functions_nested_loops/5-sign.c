0c77374c02612930de035fb2a224f360cfcbf144
German <gerchureloco@gmail.com>
2023-10-12 10:25:28 -0700

    12/10/2023

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_sign - excercise five
+ * @n: char
+ *
+ * Description: functions nested loops
+ *
+ * Return: does not return nothing at all
+ */
+
+int print_sign(int n)
+	{
+		int result = 0;
+		int printing = 0;
+
+		if (n > 0)
+		{
+			result = 1;
+			_putchar ('+');
+		}
+		if (n < 0)
+		{
+			result = -1;
+			_putchar ('-');
+		}
+		if (n == 0)
+		{
+			result = 0;
+			_putchar ('n');
+		}
+		return (result);
+}
