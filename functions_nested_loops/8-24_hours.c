b2b9769ad4b2fca066a7286f8d841d3455fb9a9d
German <gerchureloco@gmail.com>
2023-10-13 07:17:15 -0700

    13/10/2023

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * jack_bauer - excercise
+ *
+ * Description: Absolute Value
+ *
+ * Return: does not return nothing at all
+ */
+
+int jack_bauer(void)
+{
+	int minuto;
+	int hora;
+	int minutob;
+	int horab;
+
+for (hora = 0 ; hora < 24 ; hora++)
+{
+	if (hora >= 10)
+	{
+		horab = hora / 10;
+	}
+	else
+	{
+		horab = 0;
+	}
+	for (minuto = 0; minuto < 60; minuto++)
+	{
+		if (minuto >= 10)
+		{
+			minutob = minuto / 10;
+		}
+		else
+		{
+			minutob = 0;
+		}
+	_putchar(horab + '0');
+	_putchar(hora % 10 + '0');
+	_putchar(':');
+	_putchar(minutob + '0');
+	_putchar(minuto % 10 + '0');
+	_putchar ('\n');
+	}
+
+}
+}
