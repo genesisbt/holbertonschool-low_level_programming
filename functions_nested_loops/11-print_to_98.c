7ade478e09fb8e7a100465d3ad6ce6539bb3aa04
German <gerchureloco@gmail.com>
2023-10-13 10:22:29 -0700

    13/10/2023

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@
 int print_to_98(int n)
 {
 if (n == 98)
-	_putchar(n + '0');
+	printf("%d", n);
 if (n < 98)
 {
 	for (; n <= 98 ; n++)
 	{
-		_putchar(n + '0');
+		printf("%d", n);
 		if (n != 98)
 		{
 			_putchar(',');
@@ -28,7 +28,7 @@ if (n > 98)
 {
 	for (; n >= 98 ; n--)
 	{
-		_putchar(n + '0');
+		printf("%d", n);
 		if (n != 98)
 		{
 			_putchar (',');
