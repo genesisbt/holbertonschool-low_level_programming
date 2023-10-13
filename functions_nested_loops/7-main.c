0843e5a378a992c2407f59fdf4c02eb308668097
German <gerchureloco@gmail.com>
2023-10-13 05:57:26 -0700

    13/10/2023

---
+++ functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - excercise five
+ * @n: char
+ *
+ * Description: Absolute Value
+ *
+ * Return: does not return nothing at all
+ */
+
+int print_last_digit(int n)
+	{
+		n = n % 10;
+		return (n);
+	}
