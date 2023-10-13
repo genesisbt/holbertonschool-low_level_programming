fb7fc40b334ffd4ba5ddb024b5ccd93ed9ad82a6
German <gerchureloco@gmail.com>
2023-10-13 09:36:31 -0700

    13/10/2023

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ for (num1 = 0 ; num1 < 10 ; num1++)
 	for (num2 = 0; num2 < 10; num2++)
 	{
 		resultado = num1 * num2;
-		if (resultado > 10)
+		if (resultado >= 10)
 		{
 			d1 = resultado / 10;
 			_putchar(d1 + '0');
