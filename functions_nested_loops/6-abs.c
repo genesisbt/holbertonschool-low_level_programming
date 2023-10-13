52259845f4d70c21fb5d636910211662396ad17f
German <gerchureloco@gmail.com>
2023-10-13 05:45:15 -0700

    13/10/2023

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - excercise five
+ * @n: char
+ *
+ * Description: Absolute Value
+ *
+ * Return: does not return nothing at all
+ */
+
+int _abs(int n)
+	{
+		if (n < 0)
+			n = n * -1;
+	}
+	return (n);
+}
