b511156fccc63de0873868ec274789e4ded48152
german <gerchureloco@gmail.com>
2023-10-17 06:08:15 -0700

    Excercise

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - excercise five variables
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void print_most_numbers(void)
+{
+int a = 0;
+
+for (a = 0; a < 10; a++)
+{
+	if (a != 2 && a != 4)
+_putchar(a + '0');
+}
+_putchar('\n');
+}
