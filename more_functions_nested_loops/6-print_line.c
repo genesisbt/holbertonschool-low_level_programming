9fa9d274d2c41933c96370a13ca638bd934400c7
german <gerchureloco@gmail.com>
2023-10-17 06:52:18 -0700

    Excercise

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - excercise five variables
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+int print_line(int n)
+{
+int a;
+
+for (a = 0; a < n; a++)
+{
+	_putchar('_');
+}
+_putchar('\n');
+}
