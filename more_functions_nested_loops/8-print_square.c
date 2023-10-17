742b5ba230a700e3139a3c40f4ed237701e018ad
german <gerchureloco@gmail.com>
2023-10-17 07:19:46 -0700

    Excercise

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_square - excercise five variables
+ * @size: first input
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void print_square(int size)
+{
+int a;
+int b;
+
+for (a = 0; a < size; a++)
+	{
+	for (b = 0; b < size; b++)
+		{
+		_putchar('#');
+		}
+	_putchar('\n');
+	}
+if (n <= 0)
+	_putchar('\n');
+}
