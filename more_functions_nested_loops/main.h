fca2b7f35b17c31236139f0b1019081ecc5ae959
german <gerchureloco@gmail.com>
2023-10-17 05:37:12 -0700

    Excercise

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -3,17 +3,15 @@
 #define _MAIN_H_
 
 int _putchar(char c);
-int imprimir(void);
-int print_alphabet(void);
-int print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int n);
-int print_last_digit(int n);
-int jack_bauer(void);
-int times_table(void);
-int add(int a, int b);
-int print_to_98(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
 
 #endif
