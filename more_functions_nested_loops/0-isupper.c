bb2f931a7da4841c9055493b1c265fd724751ff7
german <gerchureloco@gmail.com>
2023-10-17 05:24:10 -0700

    Excercise

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - excercise five variables
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+int main(int c)
+{
+int b;	
+
+	if (c >= 'A' && c <= 'B')
+{
+	b = 1;
+}
+else
+{
+	b = 0;
+}
+return (0);
+}
