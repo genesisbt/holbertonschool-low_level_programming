3fc1285b1057d30ac3e272ee33e370bacdeea644
german <gerchureloco@gmail.com>
2023-10-17 07:38:19 -0700

    Excercise

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,10 @@ int a;
 for (a = 1; a <= 100; a++)
 	{
 	if (a % 3 == 0 && a % 5 == 0)
-		printf("FizzBuzz ");
+		if (a == 100)
+			printf ("FizzBuzz";
+		else
+			printf("FizzBuzz ");
 	else if (a % 5 == 0)
 		printf("Buzz ");
 	else if (a % 3 == 0)
