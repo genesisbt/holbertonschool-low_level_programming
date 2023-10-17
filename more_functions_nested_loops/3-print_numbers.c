30c07a8377bfe72dfbe106408e2ef3a7314b7474
german <gerchureloco@gmail.com>
2023-10-17 05:59:19 -0700

    Excercise

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_numbers - excercise five variables
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void print_numbers(void)
+{
+int a = 0;
+
+for (a = 0; a < 10; a++)
+{
+_putchar(a + '0');
+}
+_putchar('\n');
+return (0);
+}
