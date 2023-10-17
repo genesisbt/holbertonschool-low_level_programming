77dfaccad2895f6491b942cd7147f46a038c6561
german <gerchureloco@gmail.com>
2023-10-17 07:31:00 -0700

    Excercise

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - excercise five variables
+ * @size: first input
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void print_square(void)
+{
+int a;
+
+for (a = 1; a < 100; a++)
+	{
+	if (a % 3 == 0 && a % 5 == 0)
+		printf("FizzBuzz ");
+	else if (a % 5 == 0)
+		printf("Buzz ");
+	else if (a % 3 == 0)
+		printf("Fizz ");
+	else
+		printf("%d ", a)
+	}
+printf ("\n");
+}
