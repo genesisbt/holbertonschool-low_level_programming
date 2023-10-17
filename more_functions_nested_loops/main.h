b38acb6e5d2aaa40e312a7fa5405c45dd7b27a84
german <gerchureloco@gmail.com>
2023-10-17 05:30:21 -0700

    Excercise

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+int imprimir(void);
+int print_alphabet(void);
+int print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+int jack_bauer(void);
+int times_table(void);
+int add(int a, int b);
+int print_to_98(int n);
+
+#endif
