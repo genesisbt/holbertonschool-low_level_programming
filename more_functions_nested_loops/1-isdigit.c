2943628ad03ed4cd0f10f286a092bbf6e4b0b986
german <gerchureloco@gmail.com>
2023-10-17 05:49:10 -0700

    Excercise

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -11,13 +11,13 @@ int _isdigit(int c)
 {
 int b;
 
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	if (c >= '0' && c <= '9')
+	{
+	b = 1;
+}
+else
 {
 	b = 0;
-}
-else if (c >= 0 && c <= 9)
-{
-	b = 1;
 }
 return (b);
 }
