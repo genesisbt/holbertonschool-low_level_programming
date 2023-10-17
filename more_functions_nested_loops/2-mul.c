147025cac1613f03a569e09c9650b15c2a6b979a
german <gerchureloco@gmail.com>
2023-10-17 05:53:08 -0700

    Excercise

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - excercise five variables
+ * @c: entry one
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
