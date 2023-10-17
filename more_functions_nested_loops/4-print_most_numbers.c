2dd190ba81bdb5a39d14748cb8dd7f7cd88d9c4a
german <gerchureloco@gmail.com>
2023-10-17 06:18:05 -0700

    Excercise

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -12,11 +12,11 @@ void print_most_numbers(void)
 int a = 0;
 
 for (a = 0; a < 10; a++)
-{
+	{
 	if (a != 2 && a != 4)
 		{
-			_putchar(a + '0');
+		_putchar(a + '0');
 		}
-		}
+	}
 _putchar('\n');
 }
