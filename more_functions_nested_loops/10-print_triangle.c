a34d429f528a9ea839b3108032d6a0041bbf9e9a
german <gerchureloco@gmail.com>
2023-10-17 07:54:55 -0700

    Excercise

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_square - excercise five variables
+ * @size: first input
+ *
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void print_square(int size)
+{
+int a;
+int b;
+int c;
+
+for (a = 1 ; a <= size ; a++)
+	{
+	for (c = 1 ; c <= size c++)
+		println(" ");
+	for (b = size - 1; b > 0 ; b--)
+		println("#");
+	printf("\n");
+	}
+if (size <= 0)
+	_putchar('\n');
+}
