7951938af31482bba3618ff806654e749e88e85a
german <gerchureloco@gmail.com>
2023-10-18 10:32:23 -0700

    Excercise

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - excercise name
+ * @n: entry one
+ * @a: entry two
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+void print_array(int *a, int n);
+{
+	int contador;
+
+	for (contador = 0 ; contador <= n; contador++)
+	{
+	print ("%d, ", a[contador]);
+	}
+}
