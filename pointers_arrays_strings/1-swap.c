ce5d38f88347cfd1b52a153c2903f02d5eaae18a
german <gerchureloco@gmail.com>
2023-10-18 07:00:47 -0700

    Excercise

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - excercise five variables
+ * @a: entry one
+ * @b: entry two
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	*a = *b;
+	*b = c;
+}
