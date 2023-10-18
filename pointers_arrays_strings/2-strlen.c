a340c6fd6cae6895e85d1102d55d622234b02566
german <gerchureloco@gmail.com>
2023-10-18 07:26:39 -0700

    Excercise

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - excercise name
+ * @s: entry one
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (char[i] != null)
+	{
+		i++;
+	}
+	return (i);
+}
