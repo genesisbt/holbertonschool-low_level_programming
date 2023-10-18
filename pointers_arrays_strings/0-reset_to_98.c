aebddc2029a13c273506efe2ab19577c5503b6c2
german <gerchureloco@gmail.com>
2023-10-18 06:42:29 -0700

    Excercise

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - excercise five variables
+ * @n: entry one
+ * Description: one to ten
+ *
+ * Return: does not return nothing at all
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+return (b);
+}
