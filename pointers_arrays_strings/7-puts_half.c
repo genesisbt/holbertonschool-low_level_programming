cc0ccb909d71191acdfef13a6d5b6a20a7cd0f6c
german <gerchureloco@gmail.com>
2023-10-18 09:31:23 -0700

    Excercise

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - excercise name
+ * @str: entry one
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+void puts_half(char *str)
+{
+	int lenght = 0;
+	int counter = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+	lenght--;
+	if (lenght % 2 != 0)
+		lenght--;
+	for (counter = 0 ; counter <= lenght / 2)
+	{
+		_putchar(str[counter]);
+	}
+	_putchar ('\n');
+}
