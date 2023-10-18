0e11d547a83bcd1a922c663fd60a93fc884cfc4c
german <gerchureloco@gmail.com>
2023-10-18 07:39:39 -0700

    Excercise

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _puts - excercise name
+ * @str: entry one
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar (s[i]);
+		i++;
+	}
+	_putchar ('\n');
+}
