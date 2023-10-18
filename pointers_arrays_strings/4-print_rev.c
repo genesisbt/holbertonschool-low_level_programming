fb3647262c8bf1b55fb1f87e29ebfef4ac6bdd4a
german <gerchureloco@gmail.com>
2023-10-18 07:45:04 -0700

    Excercise

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_rev - excercise name
+ * @s: entry one
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--
+	}
+	_putchar('\n');
+}
