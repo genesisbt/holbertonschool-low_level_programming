71dd2b84a3523dd5d1b4e9400bc619aaa925b5c4
german <gerchureloco@gmail.com>
2023-10-20 09:17:34 -0700

    20/10/2023

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -22,7 +22,9 @@ int _strcmp(char *s1, char *s2)
 	while (ls1 == ls2 && contador <= ls1)
 	{
 		if (s1[contador] != s2[contador])
-			return (1);
+			return (13);
+		if (s2[contador] != s2[contador])
+			return (-13);
 		contador++;
 	}
 	if (ls1 != ls2)
