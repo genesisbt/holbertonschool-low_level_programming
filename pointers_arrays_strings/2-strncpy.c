6c3852b67a61af02350575252e2a8feee5116ae8
german <gerchureloco@gmail.com>
2023-10-19 21:04:33 -0700

    20/10/2023

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+int contar(char *cadena);
+/**
+ * _strncat - excercise name
+ * @src: entry one
+ * @dest: entry two
+ * @n: entry three
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int lsrc = 0;
+	int ldest = 0;
+	int contador = 0;
+
+	lsrc = contar(src);
+	ldest = contar(dest);
+	for (contador = 0 ; contador < n && contador < lsrc ; contador++)
+
+	{
+		dest[ldest + contador] = src[contador];
+	}
+return (dest);
+}
+
+/**
+ * contar - cuenta largoe
+ * @cadena: puntero a array de char
+ *
+ * Description: cuego del string
+ *
+ * Return: lenght of string
+ */
+
+int contar(char *cadena)
+{
+	int length = 0;
+
+	while (cadena[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
