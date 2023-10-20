83165c893aeb787105792e7dea80119c538589c2
german <gerchureloco@gmail.com>
2023-10-19 20:47:42 -0700

    20/10/2023

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -18,8 +18,8 @@ char *_strncat(char *dest, char *src, int n)
 	int ldest = 0;
 	int contador = 0;
 
-	lsrc = contar (src);
-	ldest = contar (dest);
+	lsrc = contar(src);
+	ldest = contar(dest);
 	for (contador = 0 ; contador < n && contador < lsrc ; contador++)
 
 	{
