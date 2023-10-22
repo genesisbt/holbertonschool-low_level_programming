15b3bd3a5d2c6ddf09b2bb7694323b5fde57bb7f
german <gerchureloco@gmail.com>
2023-10-22 08:44:05 -0700

    22/10/2023

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -14,8 +14,14 @@ void reverse_array(int *a, int n)
 {
 	int tmp = 0;
 	int contador = 0;
+	int mitad = 0;
 
-	for (contador = 0 ; contador < n ; contador++)
+	if (n % 2 == 0)
+		mitad = n / 2;
+	else
+		mitad = ( n - 1 ) / 12;
+
+	for (contador = 0 ; contador < mitad ; contador++)
 	{
 		tmp = a[contador];
 	a[contador] = a[n - 1 - contador];
