c0d8a6c6d964e0c65fa7e63fa350b8bec19b9777
german <gerchureloco@gmail.com>
2023-10-23 05:10:03 -0700

    23/10/2023

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+int contar(char *cadena);
+/**
+ * _strchr - excercise name
+ * @s: entry one
+ * @c: entry two
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+char *_strchr(char *s, char c)
+{
+int contador = 0;
+
+	for (contador = 0 ; s[contador] != '\0' ; contador++)
+	{
+		if (s[contador] == c)
+			return (s[contador]);
+	}
+return ('\0');
+}
