cd205e757a752324265aafc3fdb231dee93624e7
german <gerchureloco@gmail.com>
2023-10-22 17:59:50 -0700

    22/10/2023

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * memset - excercise name
+ * @s: entry one
+ * @b: entry two
+ * @n: entry three
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+int c = 0;
+
+for (c = 0 ; c < 95 ; c++)
+	s[s] = b;
+return (s);
+}
