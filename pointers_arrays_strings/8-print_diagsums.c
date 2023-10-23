c8006595b9da4bc52685b1b84b7624192918a4ea
german <gerchureloco@gmail.com>
2023-10-23 08:51:47 -0700

    23/10/2023

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - excercise name
+ * @a: entry one
+ * @size: entry two
+ * Description: pointers arrays strings
+ *
+ * Return: something
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int contador = 0;
+
+	contador3 = size - 1;
+	for (contador = 0 ; contador < size ; contador++)
+	{
+		printf("%d, %d\n", a[contador][contador], a[contador][contador - 1]);
+	}
+
+		
+	_putchar ('\n');
+	}
+}
