b6d583a4ea4c8ec36a0a69896110f4fd92707322
german <gerchureloco@gmail.com>
2023-10-22 17:33:49 -0700

    22/10/2023

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+int contar(char *cadena);
+/**
+ * cap_string - excercise name
+ * @str: entry one
+ *
+ * Description: pointers arrays strings
+ *
+ * Return: lenght of string
+ */
+char *leet(char *str)
+{
+	char letras[] = "aAeEoOtTlL";
+	char numeros[] = "4433007711";
+	int contador;
+	int contador2;
+
+	for (contador = 0 ; str[contador] != '\0' ; contador++)
+	{
+		for (contador2 = 0 ; letras[contador2] != '/0' ; contador2++)
+		{
+			if (str[contador] == letras[contador2])
+				str[contador] = numeros[contador2];
+		}
+	}
+}
+return (str);
+}
