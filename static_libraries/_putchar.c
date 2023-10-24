ddf7692ffe57fdbb902d633db66fc2bb5a2d5bc3
german <gerchureloco@gmail.com>
2023-10-24 14:37:56 -0700

    24/10/2023

--- static_libraries/_putchar.c
+++ static_libraries/_putchar.c
@@ -1,34 +1,14 @@
 #include "main.h"
-int imprimir(void);
 /**
- * main - excercise zero
- *
- * Description: firs use of header files
+ * _isupper - excercise five variables
+ * @c: entry one
+ * Description: one to ten
  *
  * Return: does not return nothing at all
  */
-int main(void)
+
+int _putchar(char c)
 {
-	imprimir();
-	return (0);
-}
-/**
- * imprimir - excercise zero
- *
- * Description: firs use of header files
- *
- * Return: does not return nothing at all
- */
-int imprimir(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+
+return (c);
 }
