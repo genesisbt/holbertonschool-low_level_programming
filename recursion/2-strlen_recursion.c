8cbf7fcb8f90fcbc486829b3844466975e5abc1e
german <gerchureloco@gmail.com>
2023-10-24 05:53:48 -0700

    24/10/2023

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Function name
+ * @s: entry one
+ *
+ * description: Recursion Excercises
+ *
+ * return: something
+ */
+
+void _strlen_recursion(char *s)
+{
+	int = counter;
+
+if (*s != '\0')
+{
+	counter += _print_rev_recursion(s + 1);
+	return (counter);
+}
+}
