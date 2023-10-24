b6ff63f5928c5525e17b99aba4f04e3cf128ac4c
german <gerchureloco@gmail.com>
2023-10-24 06:04:02 -0700

    24/10/2023

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -12,11 +12,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int counter = 1;
+	int counter = 0;
 
 if (*s != '\0')
 	{
-	counter += _strlen_recursion(s + 1);
+	counter = 1 + _strlen_recursion(s + 1);
 	}
 return (counter);
 }
