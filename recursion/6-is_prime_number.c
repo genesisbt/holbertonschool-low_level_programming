2c3e6cbfe88350d9ce26523f2b6cbe10f2545cb7
german <gerchureloco@gmail.com>
2023-10-24 10:30:02 -0700

    24/10/2023

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+int _prime_finder(int n, int l);
+/**
+ * _is_prime_number - Function name
+ * @n: entry one
+ *
+ * description: Recursion Excercises
+ *
+ * Return: root
+ */
+
+int _is_prime_number(int n)
+{
+if (n <= 1)
+	return (0);
+
+else
+	return (_prime_finder(n, n - 1));
+}
+
+/**
+ * _prime_finder - Function name
+ * @n: entry one
+ * @l: entry two
+ *
+ * description: Recursion Excercises
+ *
+ * Return: finds root
+ */
+int _prime_finder(int n, int l)
+{
+if (n % l == 0)
+	return (1);
+if (l < 2)
+	return (0);
+else
+	_prime_finder(n, l - 1);
+}
