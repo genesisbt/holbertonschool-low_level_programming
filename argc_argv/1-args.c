22f44bbcc1ffc4e2796c2cb5c56cb4ba46a5ae3b
german <gerchureloco@gmail.com>
2023-10-24 18:06:48 -0700

    24/10/2023

---
+++ argc_argv/1-args.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * argcount - Name of the script
+ * @argc: entry one
+ *
+ * Description: Prints the first argument of the command line in the terminal
+ *
+ * Return: is void
+ */
+
+void argcount(int argc)
+{
+	printf("%i\n", argc);
+}
+
+/**
+ * main - name
+ * @argc: entry one
+ * @argv: entry two
+ *
+ * Description: Main function
+ *
+ * Return: Zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	argcount(argc);
+	return (0);
+}
+
