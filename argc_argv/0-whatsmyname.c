96802b4ee01997cafc778e30990315a80dd67647
german <gerchureloco@gmail.com>
2023-10-24 17:33:30 -0700

    24/10/2023

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mynameis - Name of the script
+ * @argc: entry one
+ * @argv: entry two
+ *
+ * Description: Prints the first argument of the command line in the terminal
+ *
+ * Return: is void
+ */
+
+void mynameis(argc, *argv[])
+{
+	printf("%c", argv[0]);
+}
---
+++ argc_argv/main.h
@@ -0,0 +1,8 @@
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char c);
+void mynameis(int argc, char argv*[]);
+	
+#endif
