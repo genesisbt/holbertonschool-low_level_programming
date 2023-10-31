ee9aa69d7fb5f1592cc2064d6368145ceaa425ee
german <gerchureloco@gmail.com>
2023-10-30 21:43:13 -0700

    31/10/2023

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * multiply - Name of the script
+ * @n1: entry one
+ * @n2: entry two
+ * @argc: entry three
+ *
+ * Description: Prints the first argument of the command line in the terminal
+ *
+ * Return: is void
+ */
+
+int multiply(int n1, int n2, int argc)
+{
+int result;
+if (argc == 3)
+	{
+	result = n1 * n2;
+	printf("%i\n", result);
+	}
+else
+	{
+	printf("Error");
+	return (1);
+	}
+}
+
+/**
+ * main - name
+ * @argc: entry one
+ * @n1: entry two
+ * @n2: entry three
+ *
+ * Description: Main function
+ *
+ * Return: Zero
+ */
+
+int main(int argc, int n1, int n2)
+{
+	multiply(n1, n2, argc);
+	return (0);
+}
+
