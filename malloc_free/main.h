1d5abae28fba85a0b1f238e575955dc922e8e2d8
german <gerchureloco@gmail.com>
2023-11-01 06:26:24 -0700

    1/11/2023

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-#include<stdio.h>
 #include<stdlib.h>
+#include<stdio.h>
+#include<stddef.h>
 int _putchar(char c);
 char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
