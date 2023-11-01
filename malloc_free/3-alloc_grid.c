4a17502f582c50c47d394ba096ce975eb78d0e2d
german <gerchureloco@gmail.com>
2023-11-01 16:57:51 -0700

    01/11/2023

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Name of the script
+ * @width: entry one
+ * @height: entry two
+ *
+ * Description: Prints the first argument of the command line in the terminal
+ *
+ * Return: is void
+ */
+
+int **alloc_grid(int width, int height)
+{
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int c1, c2;
+		int **grid;
+
+		grid = malloc(sizeof(int *) * height);
+		if (grid == NULL)
+			return (NULL);
+		for (c1 = 0 ; c1 < height ; c1++)
+		{
+			grid[c1] = malloc(sizeof(int) * width);
+			if (grid[c1] == NULL)
+				return (NULL);
+		}
+		for (c1 = 0 ; c1 < height ; c1++)
+		{
+			for (c2 = 0 ; c2 < width ; c2++)
+			{
+				grid[c1][c2] = 0;
+			}
+		}
+		return (grid);
+	}
+}
