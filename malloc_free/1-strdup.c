842e0208d5daa89be644fc47384ffe35cfbb71a3
german <gerchureloco@gmail.com>
2023-11-01 08:33:23 -0700

    1/11/2023

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - Name of the script
+ * @str: entry one
+ *
+ * Description: Prints the first argument of the command line in the terminal
+ *
+ * Return: is void
+ */
+
+char *_strdup(char *str)
+{
+int c1, c2;
+int *copy;
+
+if (str == NULL)
+	return (NULL);
+else
+	{
+	while (str[c1] != '\0')
+		{
+		c1++
+		}
+
+	copy = malloc(sizeof(char) * (c1 + 1))
+
+	for (c2 = 0 ; c2 <= c1 ; c2++)
+		{
+		copy[c2] = str[c2];
+		}
+	copy[c2] = '\0';
+	return (copy);
+	}
+}
