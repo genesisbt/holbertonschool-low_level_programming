ee9d04b3d459ed968a27cd49b3b917f55b3fe0ac
german <gerchureloco@gmail.com>
2023-11-01 18:15:48 -0700

    01/11/2023

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Name of the script
+ * @grid: entry one
+ * @height: entry two
+ *
+ * Description: Prints the first argument of the command line in the terminal
+ *
+ * Return: is void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int c1;
+
+	for (c1 = 0 ; c1 < height ; c1++)
+	{
+			free(grid[c1]);
+	}
+}
