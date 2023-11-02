f89459c3b0483ed71b72bb53bafa308a6ab1f1fc
german <gerchureloco@gmail.com>
2023-11-02 11:08:36 -0700

    02/11/2023

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Name of the script
+ * @size: entry one
+ * @nmemb: entry two
+ *
+ * Description: description of the file
+ *
+ * Return: char pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int c = 0;
+	char *ptr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	for (c = 0 ; c < nmemb * size ; c++)
+	{
+		ptr[c] = 0;
+	}
+	return (ptr);
+}
