42e4208957c5ffcb9083ab4e08a96faab2da804d
german <gerchureloco@gmail.com>
2023-11-02 12:04:37 -0700

    02/11/2023

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -11,14 +11,14 @@
  * Return: char pointer
  */
 
-int *array_range(int min, int max);
+int *array_range(int min, int max)
 {
 	int c = 0;
 	int *ptr;
 
 	if (min > max)
 		return (NULL);
-	ptr = malloc((max - min) * sizeof(int));
+	ptr = malloc((max - min + 1) * sizeof(int));
 	if (ptr == NULL)
 		return (NULL);
 for (c = 0 ; c + min <= max ; c++)
