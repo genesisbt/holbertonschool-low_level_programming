6b5a34da450921553701731c4fc4fbf0c1f46e7b
german <gerchureloco@gmail.com>
2023-11-06 11:28:23 -0800

    6/11/2023

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - the struct name
+ * @name: its name
+ * @age: its age
+ * @owner: its owner;
+ *
+ * Description: creates a dog with those parameters;
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
