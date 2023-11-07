e917e5ef96424d4587692d86cd2670e85eeed605
german <gerchureloco@gmail.com>
2023-11-07 13:04:49 -0800

    07/11/2023

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include"function_pointers.h"
+
+/**
+ * int_index - Function name
+ * @array: first input
+ * @size: second input
+ * @cmp: third input
+ *
+ * Description: prints a name
+ *
+ * Return: something
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+size_t a;
+
+if (size <= 0)
+	return (-1);
+if (cmp != NULL && array != NULL)
+{
+	for (a = 0 ; a < size ; a++)
+	{
+	if (cmp(array[a]) != 0)
+		return (a);
+	}
+}
+}
