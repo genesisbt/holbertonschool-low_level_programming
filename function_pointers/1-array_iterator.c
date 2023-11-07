fed9eede4630e5eedf0cfae83ac8c1ad7f2d3660
german <gerchureloco@gmail.com>
2023-11-07 11:10:59 -0800

    07/11/2023

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include"function_pointers.h"
+
+/**
+ * array_iterator - Function name
+ * @array: first input
+ * @size: second input
+ * @action: third input
+ *
+ * Description: prints a name
+ *
+ * Return: something
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int a;
+
+if (action != NULL)
+{
+	for (a = 0 ; a < size ; a++)
+	{
+		action(array[a]);
+	}
+}
+}
