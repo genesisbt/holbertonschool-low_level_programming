18eb394a518b2f4bcc38193a060d6ea29c5968f8
german <gerchureloco@gmail.com>
2023-11-07 11:15:05 -0800

    07/11/2023

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -16,7 +16,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t a;
 
-if (action != NULL || array != NULL)
+if (action != NULL && array != NULL)
 {
 	for (a = 0 ; a < size ; a++)
 	{
