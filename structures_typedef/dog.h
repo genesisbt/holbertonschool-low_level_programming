4efa4d01efcee5c185e73e6eb55fb954b454f3d6
german <gerchureloco@gmail.com>
2023-11-06 16:40:41 -0800

    6/11/2023

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,13 +8,14 @@
  *
  * Description: creates a dog with those parameters;
  */
+typedef
 
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
