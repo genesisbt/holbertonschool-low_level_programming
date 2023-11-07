e250a3d06ecf698e27f3fd829fe981212aa801e0
german <gerchureloco@gmail.com>
2023-11-06 16:23:48 -0800

    6/11/2023

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -13,8 +13,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-	printf("Name: %s", d->name);
-	printf("Age: %f", d->age);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s", d->owner);
 	}
 }
