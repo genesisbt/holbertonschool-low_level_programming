11a221d7c4ecd219d4f3a1aa99021d83d4434576
german <gerchureloco@gmail.com>
2023-11-06 16:05:45 -0800

    6/11/2023

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * init_dog - name
+ * @d: first field
+ * @name: second field
+ * @age: third
+ * @owner: fourth
+ *
+ * Description: initialize a variable of type struct dog
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
