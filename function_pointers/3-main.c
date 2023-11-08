4bb20bb8615b3d1512f3b7d0d3c28b057c159e9b
german <gerchureloco@gmail.com>
2023-11-07 18:06:09 -0800

    07/11/2023

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -14,13 +14,31 @@
 
 int main(int argc, char *argv[])
 {
-(void)argc;
 int a, b, result;
 
+if (argc != 4)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+{
+	printf("Error\n");
+	exit(100);
+}
+if (argv[2][0] == '+' || argv[2][0] == '-' || argv[2][0] == '/'
+	|| argv[2][0] == '%' || argv[2][0] == '*' || argv[2][0] == '/')
+{
 a = atoi(argv[1]);
 b = atoi(argv[3]);
 result = get_op_func(argv[2])(a, b);
-
 printf("%i\n", result);
+}
+else
+{
+	printf("Error\n");
+	exit(99);
+}
 return (0);
 }
