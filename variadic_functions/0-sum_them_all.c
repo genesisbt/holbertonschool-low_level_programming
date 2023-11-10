549f1cf2aeb9b10d1411ef80dddf25537f72f30c
german <gerchureloco@gmail.com>
2023-11-09 20:40:05 -0800

    10/11/2023

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,13 +13,12 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-if (n == 0)
-	return (0);
-
 va_list lista;
 unsigned int i;
 int res = 0;
 
+if (n == 0)
+	return (0);
 va_start(lista, n);
 for (i = 0 ; i < n ; i++)
 {
