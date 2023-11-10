1c18ddcbbe6b03ce316fd6c634b9bb34efde2201
german <gerchureloco@gmail.com>
2023-11-09 22:37:49 -0800

    today

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,34 +13,36 @@
 
 void print_all(const char * const format, ...)
 {
-va_list lista;
-char *sep = ", ";
-int c1 = 0;
-
-va_start(lista, format);
-while (format[c1] != '\0')
-{
-	switch (format[c1])
+	while (format != NULL)
 	{
-		case 'c':
-			printf("%c", va_arg(lista, int));
-			break;
-		case 'i':
-			printf("%i", va_arg(lista, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(lista, double));
-			break;
-		case 's':
-			printf("%s", va_arg(lista, char *));
-			break;
-	}
-	if (format[c1 + 1] != '\0' && (format[c1] == 'c' || format[c1] == 'i'
-	  || format[c1] == 'f' || format[c1] == 's'))
-		printf("%s", sep);
-	c1++;
-}
-putchar('\n');
-va_end(lista);
+	va_list lista;
+	char *sep = ", ";
+	int c1 = 0;
 
+	va_start(lista, format);
+	while (format[c1] != '\0')
+	{
+		switch (format[c1])
+		{
+			case 'c':
+				printf("%c", va_arg(lista, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(lista, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(lista, double));
+				break;
+			case 's':
+				printf("%s", va_arg(lista, char *));
+				break;
+		}
+		if (format[c1 + 1] != '\0' && (format[c1] == 'c' || format[c1] == 'i'
+		  || format[c1] == 'f' || format[c1] == 's'))
+			printf("%s", sep);
+		c1++;
+	}
+	putchar('\n');
+	va_end(lista);
+	}
 }
