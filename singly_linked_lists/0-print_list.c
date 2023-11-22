890177b67e19cad71dfec1619ecf771234be99e6
german <gerchureloco@gmail.com>
2023-11-21 16:58:37 -0800

    sing linked lists 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include"lists.h"
+
+/**
+ * print_list - Function name
+ * h: first entry
+ * 
+ * Description: counts the members of a list
+ *
+ * Return: length of list
+ * */
+
+size_t print_list(const list_t *h) {
+size_t count = 0;
+
+while (h != NULL)
+{
+	if (h->str == NULL) 
+	{
+		printf("[0] (nil)\n");
+	} 
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+	count++;
+	h = h->next;
+}
+return count;
+}
