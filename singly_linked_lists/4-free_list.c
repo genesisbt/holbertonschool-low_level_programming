7429a9dab413d60e7b4736ddc7044fd1005f7704
german <gerchureloco@gmail.com>
2023-11-22 06:04:50 -0800

    today

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include"lists.h"
+
+/**
+ * free_list - Function name
+ * @head: first entry
+ * Description: counts the members of a list
+ *
+ * Return: length of list
+ */
+
+void free_list(list_t *head)
+{
+list_t *follow;
+
+while (head->next != NULL)
+{
+follow = head->next;
+free(head->str);
+free(head);
+head = follow;
+}
+free(head->str);
+free(head);
+}
