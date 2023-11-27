1b1d4bb9d02d7989c35a4710c12793e23863b1be
german <gerchureloco@gmail.com>
2023-11-26 20:50:31 -0800

    last

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -4,31 +4,42 @@
 #include"lists.h"
 
 /**
- * add_dnodeint - Function name
+ * add_dnodeint_end - Function name
  * @n: first entry
  * @head: second entry
  * Description: counts the members of a list
  *
  * Return: length of list
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 dlistint_t *new = malloc(sizeof(dlistint_t));
+dlistint_t *current;
 
 if (new == NULL)
-{	
+{
 	free(new);
 	return (NULL);
 }
 
 new->n = n;
-new->next = *head;
+new->next = NULL;
 new->prev = NULL;
-if (*head != NULL)
-	{
-	(*head)->prev = new;
-	}
-*head = new;
 
+if (*head == NULL)
+{
+	*head = new;
+}
+else
+{
+	current = *head;
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+
+	current->next = new;
+	new->prev = current;
+}
 return (new);
 }
