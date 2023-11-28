2b5b1caae8cbc621bf7834f0f2e8e7d9c784b981
german <gerchureloco@gmail.com>
2023-11-27 21:16:07 -0800

    last

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - value of index 
+ * @n: entry one
+ * @index: entry two
+ * Description: something
+ * Return: something
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8) /* Check if index is valid */
+	return (-1);
+return ((n >> index) & 1);
+}
