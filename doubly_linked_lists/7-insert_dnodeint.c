f3254c083ce60cf6af04e5aba128dce942dc6e15
german <gerchureloco@gmail.com>
2023-11-27 17:54:07 -0800

    last

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include"lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * insert_dnodeint_at_index - Function name
+ * @h: first entry
+ * @idx: second entry
+ * @n: third entry
+ * Description: counts the members of a list
+ *
+ * Return: length of list
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+unsigned int idxcmp = 0;
+int trigger = 0;
+dlistint_t *search = *h;
+dlistint_t *new = malloc(sizeof(dlistint_t));
+
+new->prev = NULL;
+new->next = NULL;
+new->n = n;
+if (*h != NULL)
+{
+	while (search != NULL)
+	{
+		if (idxcmp == idx)
+		{
+			search->prev->next = new;
+			new->prev = search->prev;
+			new->next = search;
+			search->prev = new;
+			trigger = 1;
+		}
+	search = search->next;
+	idxcmp++;
+	}
+}
+if (trigger == 0)
+	return (NULL);
+else
+	return (new);
+}
