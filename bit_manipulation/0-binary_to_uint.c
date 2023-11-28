aad82f15afbc6c3199abfd0f16a81304610f81af
german <gerchureloco@gmail.com>
2023-11-27 21:05:06 -0800

    last

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,16 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int number = 0;
 
 	if (b == NULL)
-		return 0;
+		return (0);
 	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
-			return 0;
+			return (0);
 		number = number << 1;
 		if (*b == '1')
 			number = number | 1;
 		b++;
 	}
-	return number;
+	return (number);
 }
 
