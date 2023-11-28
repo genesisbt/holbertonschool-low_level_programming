b838a4d5629280d6468f9b403bd7f71201b17ab8
german <gerchureloco@gmail.com>
2023-11-27 20:42:07 -0800

    last

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,52 +11,51 @@
  *
  * Return: length of list
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int idxcmp = 0;
-dlistint_t *search = *h;
-dlistint_t *new = malloc(sizeof(dlistint_t));
+	   unsigned int idxcmp = 0;
+	      dlistint_t *search = *h;
+	         dlistint_t *new = malloc(sizeof(dlistint_t));
 
-if (new == NULL)
-{
-	free(new);
-	return (NULL);
-}
-new->prev = NULL;
-new->next = NULL;
-new->n = n;
-if (idx == 0)
-{
-    new->next = *h;
-    if (*h != NULL)
-        (*h)->prev = new;
-    *h = new;
-    return (new);
-}
-else if (*h != NULL)
-{
-	while (search != NULL)
-	{
-		if (idxcmp == idx)
-		{	
-			new->prev = search->prev;
-			new->next = search;
-			if (search->prev != NULL)
-				search->prev->next = new;
-			search->prev = new;
-			return (new);
-		}
-	search = search->next;
-	idxcmp++;
-	}
-	if (search->next == NULL && idxcmp == idx)
-	{
-		new->prev = search;
-		search->next = new;
-		return (new);
-	}
-}
-free(new);
-return (NULL);
+		    if (new == NULL)
+			       {
+				              return (NULL);
+					         }
+		       new->prev = NULL;
+		          new->next = NULL;
+			     new->n = n;
+
+			        if (idx == 0)
+					   {
+						          new->next = *h;
+							         if (*h != NULL)
+									            (*h)->prev = new;
+								        *h = new;
+									       return (new);
+									          }
+				   else if (*h != NULL)
+					      {
+						             while (search != NULL)
+								            {
+										               if (idxcmp == idx)
+												                  {
+															                 new->prev = search->prev;
+																	                new->next = search;
+																			               if (search->prev != NULL)
+																					                         search->prev->next = new;
+																				                      search->prev = new;
+																						                     return (new);
+																								                }
+											                  search = search->next;
+													             idxcmp++;
+														            }
+							            if (search->next == NULL && idxcmp == idx)
+									           {
+											              new->prev = search;
+												                 search->next = new;
+														            return (new);
+															           }
+								       }
+				      free(new);
+				         return (NULL);
 }
