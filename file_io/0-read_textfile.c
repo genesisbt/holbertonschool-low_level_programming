d0b7fe93f2b0c3779b1f9f4014ef5731ba3da6f2
german <gerchureloco@gmail.com>
2023-11-29 09:06:06 -0800

    today

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -2,6 +2,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * read_textfile - name
+ * @filename: entry one
+ * @letters: entry two
+ * Description: something
+ * Return: something else
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -10,15 +17,15 @@ char *buffer;
 ssize_t bytes_read, bytes_written;
 
 if (filename == NULL)
-	return 0;
+	return (0);
 file_descriptor = open(filename, O_RDONLY);
 if (file_descriptor == -1)
-	return 0;
+	return (0);
 buffer = malloc(letters);
 if (buffer == NULL)
 {
 	close(file_descriptor);
-	return 0;
+	return (0);
 }
 bytes_read = read(file_descriptor, buffer, letters);
 bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
@@ -26,9 +33,9 @@ if (bytes_read == -1 || bytes_written == -1 || bytes_written != bytes_read)
 {
 	free(buffer);
 	close(file_descriptor);
-	return 0;
+	return (0);
 }
 free(buffer);
 close(file_descriptor);
-return bytes_written;
+return (bytes_written);
 }
