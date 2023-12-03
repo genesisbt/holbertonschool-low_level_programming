00da1a5d07273d20312c413c96822de37278dfc5
german <gerchureloco@gmail.com>
2023-12-03 08:17:50 -0800

    h

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -3,8 +3,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+/**
+ * create_file - name
+ * @filename: first
+ * @text_content: second
+ * Description: something
+ * Return: something
+ */
 
-int create_file(const char *filename, char *text_content) {
+int create_file(const char *filename, char *text_content)
+{
 int fd;
 ssize_t write_result;
 
@@ -13,13 +21,13 @@ if (filename == NULL)
 fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 if (fd == -1)
 	return (-1);
-if (text_content != NULL) 
+if (text_content != NULL)
 {
 	write_result = write(fd, text_content, strlen(text_content));
 	if (write_result == -1)
 		{
-			close(fd);
-			return (-1);
+		close(fd);
+		return (-1);
 		}
 }
 close(fd);
