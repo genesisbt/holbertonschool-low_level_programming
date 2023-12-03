95460dda050229a62e523d57483e39e5ca116ca8
german <gerchureloco@gmail.com>
2023-12-03 08:14:12 -0800

    h

---
+++ file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int create_file(const char *filename, char *text_content) {
+int fd;
+ssize_t write_result;
+
+if (filename == NULL)
+	return (-1);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+	return (-1);
+if (text_content != NULL) 
+{
+	write_result = write(fd, text_content, strlen(text_content));
+	if (write_result == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+}
+close(fd);
+return (1);
+}
