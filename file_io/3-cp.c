88383f6fc0ef92af6fad7b1ba471c5c332568459
german <gerchureloco@gmail.com>
2023-12-03 08:44:44 -0800

    h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -64,6 +64,6 @@ int main(int argc, char *argv[])
 {
 if (argc != 3)
 	print_error_and_exit(97, "Usage: cp file_from file_to");
-	copy_file(argv[1], argv[2]);
-	return (0);
+copy_file(argv[1], argv[2]);
+return (0);
 }
