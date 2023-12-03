5963c35925133c67573079913ad886c96d380685
german <gerchureloco@gmail.com>
2023-12-03 09:00:13 -0800

    h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -12,10 +12,10 @@
  * return: something
  */
 
-void print_error_and_exit(int code, const char *message)
+void print_error_and_exit(int code, const char *message, const char *filename) 
 {
-	dprintf(2, "%s\n", message);
-	exit(code);
+dprintf(2, "%s %s\n", message, filename);
+exit(code);
 }
 
 /**
@@ -27,31 +27,42 @@ void print_error_and_exit(int code, const char *message)
  */
 
 void copy_file(const char *source_file, const char *dest_file)
+
 {
 int fd_from, fd_to;
 ssize_t bytes_read, bytes_written;
 char buffer[BUFFER_SIZE];
-
+if (access(source_file, F_OK) == -1) 
+	{
+	print_error_and_exit(98, "Error: Can't read from file", source_file);
+	}
 fd_from = open(source_file, O_RDONLY);
 if (fd_from == -1)
-	print_error_and_exit(98, "Error: Can't read from file");
-fd_to = open(dest_file, O_WRONLY | O_CREAT , S_IRUSR
-		| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-if (fd_to == -1)
-{
+	{
+	print_error_and_exit(98, "Error: Can't read from file", source_file);
+	}
+fd_to = open(dest_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+if (fd_to == -1) 
+	{
 	close(fd_from);
-	print_error_and_exit(99, "Error: Can't write to file");
-}
-while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-{
+	print_error_and_exit(99, "Error: Can't write to file", dest_file);
+	}
+while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) 
+	{
 	bytes_written = write(fd_to, buffer, bytes_read);
-if (bytes_written == -1)
-	print_error_and_exit(99, "Error: Can't write to file");
-}
+	if (bytes_written == -1)
+		{
+		print_error_and_exit(99, "Error: Can't write to file", dest_file);
+		}
+	}
 if (bytes_read == -1)
-	print_error_and_exit(98, "Error: Can't read from file");
+	{
+	print_error_and_exit(98, "Error: Can't read from file", source_file);
+	}
 if (close(fd_from) == -1 || close(fd_to) == -1)
-	print_error_and_exit(100, "Error: Can't close file descriptor");
+	{
+	print_error_and_exit(100, "Error: Can't close file descriptor", NULL);
+	}
 }
 /**
  * main -name
