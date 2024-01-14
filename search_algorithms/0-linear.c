db19c594f6534f1a8c930a2be92447eeb6333c1b
genesisbt <40181430+genesisbt@users.noreply.github.com>
2024-01-14 00:23:35 -0300

    1

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,19 @@
+#include <search_algos.h>
+/**
+* linear_search - searchs a number in an array
+* @array: array of numbers to search in
+* @size: size of the array
+* @value: value of the int to search
+
+* Return: the first index where value is located
+*/
+int linear_search(int *array, size_t size, int value)
+{
+    int c;
+
+    for (c = 0 ; count1 < size - 1 ; c++)
+    {
+        if (array[c] == value)
+        return (c);
+    }    
+}
