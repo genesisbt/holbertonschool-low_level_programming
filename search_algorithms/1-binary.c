a776fa4d1744de5389c9d51ed528c44e56450447
genesisbt <40181430+genesisbt@users.noreply.github.com>
2024-01-14 02:18:09 -0300

    2

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -24,6 +24,7 @@ int recursive_binsearch(int *array, int low, int high, int value)
 		if (value > array[middle])
 			return (recursive_binsearch(array, middle + 1, high, value));
 	}
+	return (-1);
 }
 
 /**
